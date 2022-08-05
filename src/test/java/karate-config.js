243e060beb10efbddb4f6936f3efa3241a1952c8
bupadhyay <upadhyaybs@live.com>
2022-08-05 00:24:23 -0500

    Gradle and karate-config.js update

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,6 +1,6 @@
 function fn() {
     var config = {
-        baseUrl : 'https://spring-boot-api-heroku-demo.herokuapp.com'
+        baseUrl : 'https://spring-boot-api-demo-staging.herokuapp.com'
     };
     return config;
 }
